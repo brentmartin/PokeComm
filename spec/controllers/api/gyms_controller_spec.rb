6ad6f2bc1f536dc7a3fbff513a1cf45f0995f116
Brent Martin <bmart35@gmail.com>
2016-07-24 23:30:58 -0500

    wrote failing test to_not GET index of gyms that are not the specified controlling_team

--- spec/controllers/api/gyms_controller_spec.rb
+++ spec/controllers/api/gyms_controller_spec.rb
@@ -32,6 +32,20 @@ RSpec.describe Api::GymsController, :type => :controller do
         expect(response.body).to include("Homeslice Gym")
       end
 
+      it "does not return a gym that is not controlled by that team" do
+        # Set up the test's data
+        # Create a gym
+        Gym.create(name: "Homeslice Gym", controlling_team: "red")
+
+        # Call the method
+        # Hit the gyms endpoint
+        get :index, controlling_team: 'blue'
+
+        # Check the method's response to
+        # Expect that the gym that we created was returned
+        expect(response.body).to_not include("Homeslice Gym")
+      end
+
   end
 
 end
