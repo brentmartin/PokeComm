892fcb1aa51ae949831834beaa103e2305cc563f
Brent Martin <bmart35@gmail.com>
2016-07-24 23:25:00 -0500

    write rspec test to GET the index list of gyms

--- spec/controllers/api/gyms_controller_spec.rb
+++ spec/controllers/api/gyms_controller_spec.rb
@@ -2,4 +2,19 @@ require "rails_helper"
 
 RSpec.describe Api::GymsController, :type => :controller do
 
+  describe "GET /api/gyms" do
+    it "returns a gym" do
+      # Set up the test's data
+      # Create a gym
+      Gym.create(name: "Homeslice Gym")
+
+      # Call the method
+      # Hit the gyms endpoint
+      get :index
+
+      # Check the method's response to
+      # Expect that the gym that we created was returned
+      expect(response.body).to include("Homeslice Gym")
+    end
+
 end
