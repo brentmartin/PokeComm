c8e323b978647093769d4e791f7f6732162994ca
Brent Martin <bmart35@gmail.com>
2016-07-24 23:35:35 -0500

    wrote code in index to make previous test pass, refactored with a render_bad_request method under protected area

--- app/controllers/api/gyms_controller.rb
+++ app/controllers/api/gyms_controller.rb
@@ -2,12 +2,19 @@ class Api::GymsController < ApplicationController
 
   def index
     if team = params[:controlling_team]
+      (render_bad_request && return) unless ["red","blue","yellow"].include?(team)
       @gyms = Gym.where(controlling_team: team)
     else
       @gyms = Gym.all
     end
 
     render json: @gyms
+  end
+
+  protected
+
+  def render_bad_request
+    render(nothing: true, status: :bad_request)
   end
 
 end
