a5e2f12a0f36d296e5397468c5c4e58e455a5047
Brent Martin <bmart35@gmail.com>
2016-07-24 23:33:15 -0500

    wrote code in gyms controller for index to make previous test pass

--- app/controllers/api/gyms_controller.rb
+++ app/controllers/api/gyms_controller.rb
@@ -1,7 +1,12 @@
 class Api::GymsController < ApplicationController
 
   def index
+    if team = params[:controlling_team]
+      @gyms = Gym.where(controlling_team: team)
+    else
       @gyms = Gym.all
+    end
+
     render json: @gyms
   end
 
