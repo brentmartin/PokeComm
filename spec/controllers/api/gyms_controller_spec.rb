2804e72395d73eabbb5b91fb4c814c371c155d71
Brent Martin <bmart35@gmail.com>
2016-07-26 23:09:52 -0500

    refactored some for consistency

--- spec/controllers/api/gyms_controller_spec.rb
+++ spec/controllers/api/gyms_controller_spec.rb
@@ -12,18 +12,18 @@ RSpec.describe Api::GymsController, :type => :controller do
     context "when a controlling_team is specified" do
       it "returns a gym that is controlled by that team" do
         Gym.create(name: "Homeslice Gym", controlling_team: "red")
-        get :index, controlling_team: 'red'
+        get :index, controlling_team: "red"
         expect(response.body).to include("Homeslice Gym")
       end
 
       it "does not return a gym that is not controlled by that team" do
         Gym.create(name: "Homeslice Gym", controlling_team: "red")
-        get :index, controlling_team: 'blue'
+        get :index, controlling_team: "blue"
         expect(response.body).to_not include("Homeslice Gym")
       end
 
       it "throws an error if controllingTeam is invalid" do
-        get :index, controlling_team: 'orange'
+        get :index, controlling_team: "orange"
         expect(response.status).to eq(400)
       end
 
